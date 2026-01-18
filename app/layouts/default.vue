5935b1185bd48d995d334cd50fdb902e0102b03e
Beloglazov.D <git config user.name Your>
2026-01-18 11:38:14 +0300

    chore: default layouts

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <u-header />
+  <main>
+    <slot></slot>
+  </main>
+  <u-footer />
+</template>
+
+<script setup lang="ts">
+import { UFooter, UHeader } from '@/components/layouts'
+</script>
